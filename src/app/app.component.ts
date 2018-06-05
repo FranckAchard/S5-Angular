3243317266b8d2521d2508593d5f4a93897c2298
FranckAchard <36164145+FranckAchard@users.noreply.github.com>
2018-06-05 09:42:55 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,29 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'promo';
+  welcomeMsg = 'Bonjour tout le monde';
+  user = {
+    id: 0,
+    name: 'Michel François',
+    image: 'https://s3.amazonaws.com/uifaces/faces/twitter/malykhinv/128.jpg',
+    jobTitle: 'Customer Division Assistant'
+  };
+
+  testClick() {
+    console.log('coucou user : ', this.user);
+  }
+
+  testMouseOver() {
+    console.log('je vole!');
+  }
+  changeText(text) {
+    console.log('coucou : ', text);
+    this.user.name = text;
+  }
+  logInputValue(val) {
+    console.log('la valeur saisie est : ', val);
+  }
 }
